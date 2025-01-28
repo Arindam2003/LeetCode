4ff85d5b488560689854c43feed110a9d28eb522
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-01-28 22:52:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (5.4%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int number_count(int num)
+{
+    int count=0;
+    while (num)
+    {
+        num=num/10;
+        count++;
+    }
+    return count;
+}
+    int reverse(int x) {
+        int countDigit=number_count(x);   //3
+        long new_number=0;
+        while (x)
+        {
+            int rem;  //2
+            rem=x%10;  
+            new_number=new_number+rem*pow(10,countDigit-1);
+            countDigit--;
+            x=x/10;
+        }
+        if (new_number > INT_MAX || new_number < INT_MIN) {
+            return 0;
+        }
+        return (int)new_number;
+    }
+};
