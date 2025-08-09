2e4c792e8cef4c2fda75535afad09df1296a03c3
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-08-09 22:50:40 +0530

    Time: 4 ms (69.75%), Space: 71 MB (32.51%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    bool isVowel (string s,int n)
-    {
-        if((s[0]== 'a'||s[0]=='e'||s[0]=='i'||s[0]=='o'||s[0]=='u')&& (s[n-1]== 'a'||s[n-1]=='e'||s[n-1]=='i'||s[n-1]=='o'||s[n-1]=='u'))
-        {
-            return true;
+    vector<int> vowelStrings(vector<string>& words,
+                             vector<vector<int>>& queries) {
+        int n = words.size();
+        vector<int> check(n, 0);
+        auto isVowel = [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+
+        for (int i = 0; i < n; i++) {
+            if (isVowel(words[i].front()) && isVowel(words[i].back())) {
+                check[i] = 1;
+            }
         }
-        return false;
-    }
-    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries){
-        int n=words.size();
-        vector<int> vowel(n,0);
-        vector<int> prefix(n,0);
-        for(int i=0;i<n;i++)
+        vector<int> sum;
+        sum.push_back(check[0]);
+        for (int i=1;i<check.size();i++)
         {
-            if(isVowel(words[i],words[i].size()))
+            sum.push_back(sum[i-1] + check[i]);
+        }
+        vector<int> ans;
+        for(int i=0;i<queries.size();i++)
+        {
+            if(queries[i][0]>0)
             {
-                vowel[i]=1;
+                ans.push_back(sum[queries[i][1]]-sum[queries[i][0]-1]);
             }
-            else{
-                vowel[i]=0;
-            }
-        }
-        prefix[0]=vowel[0];
-        for(int i=1;i<n;i++)
-        {
-            prefix[i]=vowel[i]+prefix[i-1];
-        }
-        int m=queries.size();
-        vector <int> ans(m,0);
-        for(int i=0;i<m;i++)
-        {
-            int st=queries[i][0];
-            int end=queries[i][1];
-            if(st==0)
-            {
-                ans[i]=prefix[end];
-            }
-            else {
-                ans[i]=prefix[end]-prefix[st-1];
+            else if(queries[i][0]==0){
+                ans.push_back(sum[queries[i][1]]);
             }
         }
         return ans;
