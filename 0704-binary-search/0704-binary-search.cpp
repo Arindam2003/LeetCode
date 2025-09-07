9b08d370e077076eac8424fb6413d367f92cf9b8
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-09-07 09:06:58 +0530

    Time: 0 ms (100%), Space: 31.1 MB (98.42%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,25 +1,29 @@
 class Solution {
+    int binarySearch(vector<int>&nums,int left,int right,int target)
+    {
+        if(left==right)
+        {
+            return -1;
+        }
+        int mid=(left+right)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        bool isGreater= target>nums[mid]?true:false;
+        if(isGreater)
+        {
+            return binarySearch(nums,mid+1,right,target);
+        }
+        else{
+            return binarySearch(nums,left,mid,target);
+        }
+    }
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int left=0;
-        int right=n-1;
-        while(left<=right)
-        {
-            int mid=(left+right)/2;
-            if(nums[mid]==target)
-            {
-                return mid;
-            }
-            else if(nums[mid]<target)
-            {
-                left=mid+1;
-            }
-            else
-            {
-                right=mid-1;
-            }
-        }
-        return -1;
+        int l=0;
+        int r=nums.size();
+        sort(nums.begin(),nums.end());
+        return binarySearch(nums,l,r,target);
     }
 };
