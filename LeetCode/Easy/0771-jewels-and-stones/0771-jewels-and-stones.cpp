9b04839cba2092e6789aa148d0e8b72488e849d8
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-09-18 20:54:15 +0530

    0771-jewels-and-stones Easy 0 ms (100%)

---
+++ LeetCode/Easy/0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int cnt=0;
+        for(int i=0;i<jewels.size();i++)
+        {
+            int temp=count(stones.begin(),stones.end(),jewels[i]);
+            cnt+=temp;
+        }
+        return cnt;
+    }
+};
