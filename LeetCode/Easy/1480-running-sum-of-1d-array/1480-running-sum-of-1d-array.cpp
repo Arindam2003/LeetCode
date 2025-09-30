951afe3b5be0b1beec3aff30fe6bfbe7cbbeb03c
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-09-30 17:13:10 +0530

    1480-running-sum-of-1d-array Easy 0 ms (100%)

--- LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> ans;
-        int sum=0;
-        for(int i=0;i<nums.size();i++)
+        int temp=nums[0];
+        ans.push_back(temp);
+        for(int i=1;i<nums.size();i++)
         {
-            sum=sum+nums[i];
-            ans.push_back(sum);
+            temp+=nums[i];
+            ans.push_back(temp);
         }
         return ans;
     }
