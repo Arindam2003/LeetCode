ed8228147551970bf6b79184b3b2ac698bea13e1
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-11-02 19:05:11 +0530

    0541-reverse-string-ii Easy 0 ms (100%)

---
+++ LeetCode/Easy/0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int l=0;
+        int n=s.length();
+        int r=min(l+k,n);
+        while(l<n)
+        {
+             reverse(s.begin()+l,s.begin()+r);
+            l+=2*k;
+            r=min(l+k,n);
+        }
+        return s;
+    }
+};
