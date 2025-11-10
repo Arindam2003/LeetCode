28b42cb7d004478056a7b702d4a77c558a12013d
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-11-10 11:23:37 +0530

    0039-combination-sum Medium 2 ms (59.48%)

---
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> answer={};
+    
+    void helperCombi(vector<int>& candidates, int target,int start,vector<int>&current)
+    {
+        if(target==0)
+        {
+            answer.push_back(current);
+            return;
+        }
+        for(int i=start;i<candidates.size();i++)
+        {
+            if(target>=candidates[i])
+            {
+                current.push_back(candidates[i]);
+                helperCombi(candidates,target-candidates[i],i,current);
+                current.pop_back();
+            }else {
+                break;
+            }
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> current={};
+        sort(candidates.begin(),candidates.end());
+        helperCombi(candidates,target,0,current);
+
+        return answer;
+    }
+};
