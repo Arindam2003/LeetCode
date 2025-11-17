83f692fd196102a5bd8aef2f0ad0fb573c14de2b
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-11-17 20:48:21 +0530

    0035-search-insert-position Easy 0 ms (100%)

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.cpp
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0;
-        int high = n - 1;
-        int ans = n;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] >= target) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int n=nums.size();
+        int left =0;
+        int right=n-1;
+        int ans;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target)
+            {
+                ans=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
         }
         return ans;
