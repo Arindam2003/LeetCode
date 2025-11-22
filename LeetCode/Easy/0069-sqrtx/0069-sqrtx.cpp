83d32433c17db07c00f356d29a09b28d0ff8bc66
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-11-22 22:12:35 +0530

    0069-sqrtx Easy 0 ms (100%)

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
@@ -4,15 +4,23 @@ public:
         int low=1;
         int high=x;
         int ans=0;
+        if(x==0) 
+        {
+            return 0;
+        }
         while(low<=high)
         {
-            long mid=low+(high-low)/2;
-            if(mid*mid<=x)
+            int mid=low+(high-low)/2;
+            long long temp = 1LL * mid * mid;
+            if(temp==x)
             {
+                return mid;
+            }else if(temp>x)
+            {
+                high=mid-1;
+            }else{
                 ans=mid;
                 low=mid+1;
-            }else{
-                high=mid-1;
             }
         }
         return ans;
