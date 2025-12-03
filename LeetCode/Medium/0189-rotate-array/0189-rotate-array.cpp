14f642c5da7d45c29b812f556f20257e41dcad07
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-12-03 19:25:23 +0530

    0189-rotate-array Medium 0 ms (100%)

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        vector<int> temp1={};
-        vector<int> temp2={};
-        for(int i=0;i<nums.size()-k;i++)
-        {
-            temp1.push_back(nums[i]);
-        }
-        for(int i=nums.size()-k;i<nums.size();i++)
-        {
-            temp2.push_back(nums[i]);
-        }
-        
-        for(int i=0;i<k;i++)
-        {
-            nums[i]=temp2[i];
-        }
-        for(int i=k;i<nums.size();i++)
-        {
-            nums[i]=temp1[i-k];
-        }
-
+        int n=nums.size();
+        k %= n; 
+        reverse(nums.begin(),nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k),nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
