5188e24f0aff5bc60c43b7fc2c5e64cba77be54e
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2025-12-21 22:47:13 +0530

    0415-add-strings Easy 0 ms (100%)

--- LeetCode/Easy/0415-add-strings/0415-add-strings.cpp
+++ LeetCode/Easy/0415-add-strings/0415-add-strings.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        if(num1.size()<num2.size())
-        {
-            swap(num1,num2);
-        }
-        reverse(num1.begin(),num1.end());
-        reverse(num2.begin(),num2.end());
-        string result;
+        int n=num1.size();
+        int m=num2.size();
+
+        int i=n-1;
+        int j=m-1;
+
         int carry=0;
-        for(int i=0;i<num1.size();i++)
+        string ans;
+        while(i>=0 && j>=0)
         {
-            int digit1=num1[i]-'0';
-            int digit2;
-            if(i<num2.size())
-            {
-                digit2=num2[i]-'0';
-            }else{
-                digit2=0;
-            }
+            int n1= num1[i]-'0';
+            int n2= num2[j]-'0';
 
-            int sum=digit1+digit2+carry;
+            int final=n1+n2+carry;
 
-            result.push_back(sum%10+'0');
-            carry=sum/10;
+            carry=final/10;
+
+            ans.push_back((final%10)+'0');
+            i--;
+            j--;
         }
-        if (carry) result.push_back(carry + '0');
-        reverse(result.begin(),result.end());
-        return result;
+
+        while(i>=0)
+        {
+            int n1= num1[i]-'0';
+
+            int final=n1+carry;
+
+            carry=final/10;
+
+            ans.push_back((final%10)+'0');
+            i--;
+        }
+
+        while(j>=0)
+        {
+            int n2= num2[j]-'0';
+
+            int final=n2+carry;
+
+            carry=final/10;
+
+            ans.push_back((final%10)+'0'); 
+            j--;  
+        }
+        if (carry) {
+            ans.push_back(carry + '0');
+        }
+        reverse(ans.begin(), ans.end());
+
+        return ans;
     }
 };
