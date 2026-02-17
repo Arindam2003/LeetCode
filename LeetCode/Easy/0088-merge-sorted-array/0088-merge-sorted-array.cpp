79cf7107d00101f6292dfdbdfba2566602c240e7
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2026-02-17 08:36:02 +0530

    0088-merge-sorted-array Easy 0 ms (100%)

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int l=0;
-        int r=0;
+        int l = 0;
+        int r = 0;
         vector<int> ans;
-        while(l<m && r<n)
-        {
-            if(nums1[l]<=nums2[r])
-            {
+        while (l < m && r < n) {
+            if (nums1[l] <= nums2[r]) {
                 ans.push_back(nums1[l]);
                 l++;
-            }else{
+            } else {
                 ans.push_back(nums2[r]);
                 r++;
             }
         }
 
-        while(l<m)
-        {
+        while (l < m) {
             ans.push_back(nums1[l]);
             l++;
         }
-        while(r<n)
-        {
+        while (r < n) {
             ans.push_back(nums2[r]);
             r++;
         }
 
-        for(int i=0;i<ans.size();i++)
-        {
-            nums1[i]=ans[i];
+        for (int i = 0; i < ans.size(); i++) {
+            nums1[i] = ans[i];
         }
     }
 };
