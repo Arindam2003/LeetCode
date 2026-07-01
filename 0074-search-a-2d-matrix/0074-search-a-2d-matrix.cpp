737c04348c0281a4c37176f7897411291dfc5f75
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2026-07-01 23:04:19 +0530

    Time: 0 ms (100%), Space: 13.2 MB (98.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,45 +1,24 @@
 class Solution {
-    bool binarySearch(vector<int> row,int target)
-    {
-        int r=row.size()-1;
-        int l=0;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(target==row[mid])
-            {
-                return true;
-            }else if(target>row[mid])
-            {
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
-        } 
-        return false;
-    }
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rowFirst=0;
-        int rowLast=matrix.size()-1;
-        int colno=matrix[0].size()-1;
-        int ansRow;
-        while(rowFirst<=rowLast)
+        int r=matrix.size();
+        int c=matrix[0].size();
+
+        int i=0,j=c-1;
+
+        while(i<r && j>=0)
         {
-            int mid=rowFirst+(rowLast-rowFirst)/2;
-            if(target>=matrix[mid][0] && target<=matrix[mid][colno])
+            if(matrix[i][j]==target)
             {
-                ansRow=mid;
-                break;
+                return true;
             }
-            else if(target>matrix[mid][0])
+            else if(matrix[i][j]>target)
             {
-                rowFirst=mid+1;
-            }
-            else{
-                rowLast=mid-1;
+                j--;
+            }else{
+                i++;
             }
         }
-        return binarySearch(matrix[ansRow],target);
+        return false;
     }
 };
