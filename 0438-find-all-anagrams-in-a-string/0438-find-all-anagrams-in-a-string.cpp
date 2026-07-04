da961188d7d716e051068e9e53716e3d3c21ccb9
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2026-07-04 10:01:58 +0530

    Time: 26 ms (18.52%), Space: 14.7 MB (20.78%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> pCount(26,0);
-        vector<int> sCount(26,0);
-        vector<int> ans;
+        unordered_map<char,int>mp1;
+        unordered_map<char,int>mp2;
+
+
         for(int i=0;i<p.size();i++)
         {
-            pCount[p[i]-'a']++;
+            mp1[p[i]]++;
         }
 
+        vector<int>ans;
         for(int i=0;i<s.size();i++)
         {
-            sCount[s[i]-'a']++;
-
+            mp2[s[i]]++;
             if(i>=p.size())
             {
-                sCount[s[i-p.size()]-'a']--;
+                mp2[s[i-p.size()]]--;
+                if(mp2[s[i-p.size()]]==0)
+                {
+                    mp2.erase(s[i-p.size()]);
+                }
             }
 
-            if(sCount==pCount)
+            if(mp1==mp2)
             {
                 ans.push_back(i-p.size()+1);
             }
         }
+
         return ans;
+
     }
 };
