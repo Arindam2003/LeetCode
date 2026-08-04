61c0adde2a2840895146d28016a895f2d6ed7c4d
Arindam Dinda <141807975+Arindam2003@users.noreply.github.com>
2026-08-04 16:42:31 +0530

    Time: 2 ms (29.17%), Space: 29 MB (71.26%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -6,26 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr || q==nullptr){
-            return p==q;
-        }
-        bool isLeftSame=isSameTree(p->left,q->left);
-        bool isRightSame=isSameTree(p->right,q->right);
-        return isLeftSame && isRightSame && (p->val==q->val);
-    }
 public:
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == nullptr || subRoot==nullptr){
-            return root==subRoot;
-        }
-        if(root->val==subRoot->val && isSameTree(root,subRoot)){
+    bool isIdentical(TreeNode* root1, TreeNode* root2) {
+        if (root1 == nullptr && root2 == nullptr) {
             return true;
         }
-        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+        if (root1 == nullptr || root2 == nullptr) {
+            return false;
+        }
+        if (root1->val != root2->val) {
+            return false;
+        }
+        return isIdentical(root1->left, root2->left) &&
+               isIdentical(root1->right, root2->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr && subRoot == nullptr) {
+            return true;
+        } 
+        if(root==nullptr || subRoot==nullptr)
+        {
+            return false;
+        }
+        if (root->val == subRoot->val) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        bool leftSub = isSubtree(root->left, subRoot);
+        if (leftSub) {
+            return true;
+        }
+        return isSubtree(root->right, subRoot);
     }
 };
